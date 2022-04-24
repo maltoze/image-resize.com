84d1098ad2679a5ad93e170550d69f9c9dad9b81
maltoze <maltoze@qq.com>
2022-04-24 17:47:49 +0800

    chore: insert adsense code

--- gatsby-ssr.ts
+++
@@ -1,3 +0,0 @@
-import wrapPageElement from './src/wrapPageElement';
-
-export { wrapPageElement };
---
+++ gatsby-ssr.tsx
@@ -0,0 +1,13 @@
+import wrapPageElement from './src/wrapPageElement';
+
+const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9027498975434534"
+      crossOrigin="anonymous"
+    ></script>,
+  ]);
+};
+
+export { onRenderBody, wrapPageElement };
