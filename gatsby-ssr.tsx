8ad912527d549a0adf6a9f12a8e599afa750972c
maltoze <maltoze@qq.com>
2022-04-25 09:14:26 +0800

    chore: remove site ads

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,13 +1,3 @@
 import wrapPageElement from './src/wrapPageElement';
 
-const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9027498975434534"
-      crossOrigin="anonymous"
-    ></script>,
-  ]);
-};
-
-export { onRenderBody, wrapPageElement };
+export { wrapPageElement };
