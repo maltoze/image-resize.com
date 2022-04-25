b5cacf1d01747f2cdcc5b33d3c6457fd46b993e3
maltoze <maltoze@qq.com>
2022-04-25 09:21:31 +0800

    chore: add ad unit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,22 @@ const IndexPage = () => {
       <div className="mx-auto mt-6 w-full max-w-3xl px-6 md:px-3">
         <Heading />
         <ImageSelector />
+
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9027498975434534"
+          crossOrigin="anonymous"
+        ></script>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-9027498975434534"
+          data-ad-slot="3547032576"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+
         {selectedImageFiles.length > 0 && <ImageResizer />}
       </div>
     </>
